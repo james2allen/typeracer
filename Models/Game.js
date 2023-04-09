03986c6ec6d61095d26dd9d83be398da3ea71a08
James <james2allen@gmail.com>
2023-04-09 15:42:22 -0400

    Initial project push

---
+++ Models/Game.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+
+const PlayerSchema = new mongoose.Schema();
---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+const mongoose = require("mongoose");
+
+const expressServer = app.listen(3001);
+const io = socketio(expressServer);
+
+mongoose.connect(
+  "mongodb://localhost:27017/typeracer",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {}
+);
