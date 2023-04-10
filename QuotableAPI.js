a6eb55687dd3d605eef253cfcdb4b93d3cc2f039
James <james2allen@gmail.com>
2023-04-10 15:05:38 -0400

    Fixed issue with QuotibleAPI so that the words array is returned to the frontend

--- QuotableAPI.js
+++ QuotableAPI.js
@@ -1,5 +1,9 @@
+const fetch = require("node-fetch");
+
 const uri = "http://api.quotable.io/random";
 
 module.exports = getData = () => {
-  return fetch(uri).then((response) => response.data.content.split(""));
+  return fetch(uri)
+    .then((response) => response.json())
+    .then((data) => data?.content?.split(""));
 };
--- app.js
+++ app.js
@@ -14,14 +14,34 @@ const Game = require("./Models/Game");
 const QuotableAPI = require("./QuotableAPI");
 
 mongoose
-  .connect("mongodb://localhost:27017/typeracer", {
+  .connect("mongodb://0.0.0.0:27017/typeracer", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("successfully connected to database");
-  });
+  })
+  .catch((error) => console.log(error));
 
-io.on("conenct", (socket) => {
-  socket.emit("test", "this is from the server");
+io.on("connect", (socket) => {
+  socket.on("create-game", async (nickName) => {
+    try {
+      const quotableData = await QuotableAPI();
+      let game = new Game();
+      game.words = quotableData;
+      let player = {
+        socketID: socket.id,
+        isPartyLeader: true,
+        nickName,
+      };
+      game.players.push(player);
+      game = await game.save();
+
+      const gameID = game._id.toString();
+      socket.join(gameID);
+      io.to(gameID).emit("update-game", game);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 });
